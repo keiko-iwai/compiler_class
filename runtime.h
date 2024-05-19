b7e5125d62fdab1b4b697570cb79e443bf7c6bba
Degifter <keiko.iwai@gmail.com>
2024-05-19 04:52:22 +0200

    runtime header

--- runtime.h
+++ runtime.h
@@ -2,9 +2,20 @@
 
 extern "C"
 {
+  /* IO */
   int printi(int X);
   int printd(double X);
   int print(const char *fmt, ...);
   int println(const char *fmt, ...);
+
+  int readi();
+  double readd();
+  char *readline();
+
+  /* MATH */
   double sqrt(double X);
+  double sin(double arg);
+  double cos(double arg);
+  double pow(double base, double exponent);
+  double pi();
 }
