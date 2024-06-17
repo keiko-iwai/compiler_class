3a40f4969ffd870eb7fcb0dc6fb0e7dfc0602514
Degifter <keiko.iwai@gmail.com>
2024-06-17 23:53:35 +0200

    Allow run a file interactively

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  if (optInputFile.empty() || isOptInteractive)
+  if (optInputFile.empty())
     yyin = stdin;
   else if (!(yyin = fopen(optInputFile.c_str(), "rt")))
   {
