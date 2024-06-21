4a0f8cbc67eebe67ce5a1d2606fc02b839204eb9
Degifter <keiko.iwai@gmail.com>
2024-06-22 00:40:49 +0200

    Run example script

--- run_example.sh
+++ run_example.sh
@@ -2,11 +2,11 @@
 echo "...................................."
 echo Compile strings.t to object file:
 echo "...................................."
-./compiler < ./tests/basic_string.t
+./compiler ./tests/basic_string.t
 echo "...................................."
 echo Compile object file with runtime:
 echo "...................................."
-clang++ runtime.cpp output.o -o string
+clang++ runtime.cpp basic_string.o -o string
 echo "...................................."
 echo Run program:
 echo "...................................."
